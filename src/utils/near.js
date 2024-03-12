f31ca65aa8f083b293c4ac173468fd55a4f6dcdf
1_aunchers <91509158+Launchers-1@users.noreply.github.com>
2024-03-11 21:49:00 -0400

    Update near.js

--- src/utils/near.js
+++ src/utils/near.js
@@ -5,15 +5,18 @@ import { formatNearAmount } from "near-api-js/lib/utils/format";
 const nearEnv = environment("testnet");
 
 export async function initializeContract() {
-  const near = await connect(
+ const near = await connect(
     Object.assign(
       { deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } },
-      nearEnv
+      nearEnv,
+      {
+        walletUrl: "https://testnet.mynearwallet.com/", // Updated wallet URL
+      }
     )
-  );
-  window.walletConnection = new WalletConnection(near);
-  window.accountId = window.walletConnection.getAccountId();
-  window.contract = new Contract(
+ );
+ window.walletConnection = new WalletConnection(near);
+ window.accountId = window.walletConnection.getAccountId();
+ window.contract = new Contract(
     window.walletConnection.account(),
     nearEnv.contractName,
     {
@@ -22,25 +25,25 @@ export async function initializeContract() {
       // List call methods that change state
       changeMethods: ["addNewMeal", "placeOrder", "deleteOrder"],
     }
-  );
+ );
 }
 
 export async function accountBalance() {
-  return formatNearAmount(
+ return formatNearAmount(
     (await window.walletConnection.account().getAccountBalance()).total,
     2
-  );
+ );
 }
 
 export async function getAccountId() {
-  return window.walletConnection.getAccountId();
+ return window.walletConnection.getAccountId();
 }
 
 export function login() {
-  window.walletConnection.requestSignIn(nearEnv.contractName);
+ window.walletConnection.requestSignIn(nearEnv.contractName);
 }
 
 export function logout() {
-  window.walletConnection.signOut();
-  window.location.reload();
+ window.walletConnection.signOut();
+ window.location.reload();
 }
